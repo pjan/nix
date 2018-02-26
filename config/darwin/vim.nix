d19757e981262248e3ae2c7093b09a486eb165ba
pjan vandaele <pjan@pjan.io>
2018-02-26 18:26:16 +0900

    fix vim config

--- config/darwin/vim.nix
+++ config/darwin/vim.nix
@@ -269,7 +269,7 @@
 
           " Fugitive {
               let g:signify_sign_show_count = 1
-              nnoremap <silent> <leader>gs :GStatus<CR>
+              nnoremap <silent> <leader>gs :Gstatus<CR>
               nnoremap <silent> <leader>gd :Gdiff<CR>
               nnoremap <silent> <leader>gc :Gcommit<CR>
               nnoremap <silent> <leader>gb :Gblame<CR>
