20f10f4e10382609ebf68f4e93c1621694a0f639
pjan vandaele <pjan@pjan.io>
2018-03-01 22:36:34 +0900

    update neco_ghc config in vim

--- config/darwin/vim.nix
+++ config/darwin/vim.nix
@@ -344,6 +344,7 @@
 
           " neco-ghc {
               let g:haskellmode_completion_ghc = 0
+              let g:necoghc_enable_detailed_browse = 1
 
               autocmd FileType haskell setlocal omnifunc=necoghc#omnifunc
           " }
