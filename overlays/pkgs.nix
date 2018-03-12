214a428d76c2a6a30583a81f34699ccd9d347f9c
pjan vandaele <pjan@pjan.io>
2018-03-12 10:10:16 +0900

    removes the gist overlay in favor of the one in nixpkgs

--- overlays/pkgs.nix
+++ overlays/pkgs.nix
@@ -3,7 +3,6 @@ self: super:
 rec {
 
   ghi          = super.callPackage ./pkgs/applications/version-management/git-and-tools/ghi { };
-  gist         = super.callPackage ./pkgs/applications/version-management/git-and-tools/gist { };
   home-manager = super.callPackage ../home-manager/home-manager {
     path = toString ../home-manager;
   };
