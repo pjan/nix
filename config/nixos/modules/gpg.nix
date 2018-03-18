370e6a242fb6e2469f14fa97198aafa6b28f577b
pjan vandaele <pjan@pjan.io>
2018-03-18 17:57:11 +0900

    update gpg module

--- config/nixos/modules/gpg.nix
+++ config/nixos/modules/gpg.nix
@@ -2,20 +2,30 @@
 
 {
 
-  programs.gnupg.agent {
-    enable = true;
-    enableSSHSupport = true;
-  };
+  #programs.gnupg.agent = {
+  #  enable = true;
+  #  enableSSHSupport = true;
+  #};
 
   programs.ssh.startAgent = false;
 
   # daemon that exposes a generic API that allows programs to communicate with smartcards
   services.pcscd.enable = true;
 
-  environment.systemPackages = [
+  environment.systemPackages = with pkgs; [
     gnupg
     yubikey-personalization
   ];
+
+  environment.extraInit = ''
+    export GPG_TTY=$(tty)
+
+    ${pkgs.gnupg}/bin/gpg-connect-agent --quiet updatestartuptty /bye > /dev/null
+    
+    if [ -z "$SSH_AUTH_SOCK" ]; then
+      export SSH_AUTH_SOCK=$(${pkgs.gnupg}/bin/gpgconf --list-dirs agent-ssh-socket)
+    fi
+  '';
 
   services.udev.packages = with pkgs; [
     yubikey-personalization
