37c4171bb1ef2705767cfa1c043e0013f0309c74
pjan vandaele <pjan@vandaele.io>
2020-06-07 23:11:50 +0800

    home config updates

--- config/home-manager/default.nix
+++ config/home-manager/default.nix
@@ -1,19 +1,20 @@
 { config, pkgs, ... }:
 
 let
-  homedir = config.home.homeDirectory;
+
+  homeDirectory = config.home.homeDirectory;
+
 in rec {
 
   xdg = {
     enable = true;
 
-    configHome = "${homedir}/.config";
-    dataHome   = "${homedir}/.local/share";
-    cacheHome  = "${homedir}/.cache";
-
-
-
+    configHome = "${homeDirectory}/.config";
+    dataHome   = "${homeDirectory}/.local/share";
+    cacheHome  = "${homeDirectory}/.cache";
   };
+
+  # programs.jq
 
   home.sessionVariables = {
     CABAL_CONFIG  = "${xdg.configHome}/cabal/config";
@@ -26,15 +27,31 @@ in rec {
   };
 
   imports = [
+    ./modules/git.nix
+    ./modules/gpg.nix
     # ./modules/ssh.nix
     # ./modules/weechat.nix
     # ./modules/wget.nix
     ../shared/modules/nixpkgs.nix
   ];
 
-	home.file.".curlrc".text = ''
-		capath=${pkgs.cacert}/etc/ssl/certs/
-		cacert=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
-	'';
+	home.file = {
+    ".curlrc".text = ''
+      capath = ${pkgs.cacert}/etc/ssl/certs/
+      cacert = ${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
+
+      user-agent = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)"
+      referer = ";auto"
+      connect-timeout = 60
+    '';
+
+    ".wgetrc".text = ''
+      ca_directory = ${pkgs.cacert}/etc/ssl/certs/
+      ca_certificate = ${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
+    '';
+  };
+
+
+  
 
 }
---
+++ config/home-manager/modules/gpg.nix
@@ -0,0 +1,38 @@
+{ config, pkgs, ... }:
+
+{
+
+  config.home.sessionVariables = {
+    GNUPGHOME     = "${config.xdg.configHome}/gnupg";
+    SSH_AUTH_SOCK = "${config.xdg.configHome}/gnupg/S.gpg-agent.ssh";
+  };
+
+  config.xdg.configFile."gnupg/gpg-agent.conf".text = ''
+    enable-ssh-support
+    default-cache-ttl 86400
+    max-cache-ttl 86400
+    pinentry-program ${pkgs.pinentry_mac}/Applications/pinentry-mac.app/Contents/MacOS/pinentry-mac
+  '';
+
+  config.xdg.configFile."gnupg/gog.conf".text = ''
+    personal-cipher-preferences AES256 AES192 AES CAST5
+    personal-digest-preferences SHA512 SHA384 SHA256 SHA224
+    personal-compress-preferences ZLIB BZIP2 ZIP Uncompressed
+    default-preference-list SHA512 SHA384 SHA256 SHA224 AES256 AES192 AES CAST5 ZLIB BZIP2 ZIP Uncompressed
+    cert-digest-algo SHA512
+    s2k-digest-algo SHA512
+    s2k-cipher-algo AES256
+    charset utf-8
+    fixed-list-mode
+    no-comments
+    no-emit-version
+    keyid-format 0xlong
+    list-options show-uid-validity
+    verify-options show-uid-validity
+    with-fingerprint
+    require-cross-certification
+    no-symkey-cache
+    use-agent
+  '';
+
+}
